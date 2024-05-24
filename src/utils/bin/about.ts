d6ccbf6a27a0d288107b16aa69cf24aa79d703fd
navanjane <nkindelpitiya@gmail.com>
2024-05-24 19:02:29 +0530

    new changes

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -1,5 +1,32 @@
 export const about = async (args: string[]): Promise<string> => {
-  const bio = `I am a DevOps engineer.`
+  const bio = `🚀  DevOps Engineer 🚀
+
+  Greetings! I am a DevOps Engineer with a fervent enthusiasm for refining development and operations processes in the dynamic world of technology. My journey in the IT realm has equipped me with a diverse skill set, allowing me to navigate and contribute effectively.
+  
+  🛠️ Technical Toolkit:
+  
+  <b>AWS Explorer</b>: ☁️ Navigating the cloud with confidence, I am proficient in leveraging Amazon Web Services (AWS) to architect, deploy, and manage scalable and resilient infrastructures.
+  <b>Terraform Trailblazer</b>: 🌐 Venturing into the realm of infrastructure as code, I wield Terraform to craft and provision infrastructure with precision and efficiency.
+  <b>Kubernetes Navigator</b>: 🚢 Navigating the seas of container orchestration, I excel in deploying and managing containerized applications efficiently using Kubernetes.
+  <b>Ansible Apprentice</b>: 🤖 Embracing the power of automation, I utilize Ansible to streamline configuration management and deployment tasks across diverse environments.
+  <b>Docker Dabbler</b>: 🐳 Exploring the realm of containerization, I design and optimize containerized workflows for enhanced efficiency and portability.
+  <b>Linux Explorer</b>: 💻 Command-line is my learning ground, and I am continually honing my skills in the Linux ecosystem to ensure robust and secure operations.
+  <b>Grafana User</b>: 📊 Leveraging Grafana for insightful data visualization and creating dashboards for monitoring and observability.
+  <b>Prometheus Operator</b>: 📈 Proficiently using Prometheus for robust monitoring and alerting, ensuring the health of applications and systems.
+  
+  🌐 Cloud Enthusiast:
+
+  My passion for cloud technologies is in its infancy, yet growing steadily. I am committed to harnessing the potential of the cloud to drive innovation, scalability, and cost-efficiency for businesses.
+  
+  💡 DevOps Devotee:
+  
+  DevOps is more than a role; it's a journey I'm embarking on with dedication. Collaborative spirit, continuous integration, and delivery are principles I'm actively incorporating to elevate organizational processes.
+  
+  Whether it's refining infrastructures, automating tasks, or troubleshooting challenges, I bring a blend of growing technical skills and a passion for efficiency to every project.
+  
+  Let's continue building, automating, and innovating together in the ever-evolving landscape of DevOps and cloud technologies!
+  
+  `
 
   return bio
 }
--- src/utils/bin/social.ts
+++ src/utils/bin/social.ts
@@ -1,16 +1,16 @@
 import config from "@/data/config.json"
 
-export const instagram = async (args: string[]): Promise<string> => {
-  window.open(`https://www.instagram.com/${config.social.instagram}/`)
+// export const instagram = async (args: string[]): Promise<string> => {
+//   window.open(`https://www.instagram.com/${config.social.instagram}/`)
 
-  return "Opening instagram..."
-}
+//   return "Opening instagram..."
+// }
 
-export const github = async (args: string[]): Promise<string> => {
-  window.open(`https://github.com/${config.social.github}/`)
+// export const github = async (args: string[]): Promise<string> => {
+//   window.open(`https://github.com/${config.social.github}/`)
 
-  return "Opening github..."
-}
+//   return "Opening github..."
+// }
 
 export const linkedin = async (args: string[]): Promise<string> => {
   window.open(`https://www.linkedin.com/in/${config.social.linkedin}/`)
@@ -18,8 +18,8 @@ export const linkedin = async (args: string[]): Promise<string> => {
   return "Opening linkedin..."
 }
 
-export const twitter = async (args: string[]): Promise<string> => {
-  window.open(`https://twitter.com/${config.social.twitter}/`)
+// export const twitter = async (args: string[]): Promise<string> => {
+//   window.open(`https://twitter.com/${config.social.twitter}/`)
 
-  return "Opening twitter..."
-}
+//   return "Opening twitter..."
+// }
